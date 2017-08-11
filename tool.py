f595a164a7f860d16a71e9492405dbf452e9fa29
Sunguk Lee <d3m3vilurr@gmail.com>
2017-08-12 02:16:50 +0900

    Update more comments

--- tool.py
+++ tool.py
@@ -2,15 +2,22 @@ import struct
 from PyCRC.CRC16 import CRC16
 
 # AK2I rom layout
-# | offset | size   | desc                                        |
-# | 0x0000 | 0x2000 | stage2 blowfish data(0x1048 bytes + padding |
-# | 0x2000 | 0x1000 | nds header                                  |
-# | 0x3000 | 0x1000 | padding                                     |
-# | 0x4000 | 0x0400 | first 0x48 bytes of blowfish data + padding |
-# | 0x4400 | 0x0BF0 | next 0xBF0 bytes of blowfish data           |
-# | 0x5000 | 0x1000 | `FF00FF00AA555AA55` magic then ...          |
-# | 0x6000 | 0x4000 | sec block                                   |
-# | 0xA000 | ...    | data block                                  |
+# | offset  | size   | desc                                        |
+# | 0x00000 | 0x2000 | lv2 blowfish data(0x1048 bytes + padding    |
+# | 0x02000 | 0x1000 | nds header                                  |
+# | 0x03000 | 0x1000 | padding                                     |
+# | 0x04000 | 0x0400 | first 0x48 bytes of blowfish data + padding |
+# | 0x04400 | 0x0BF0 | next 0xBF0 bytes of blowfish data           |
+# | 0x05000 | 0x1000 | `FF00FF00AA555AA55` magic then ...          |
+# | 0x06000 | 0x4000 | sec block                                   |
+# | 0x0A000 | ...    | data block                                  |
+# | 0x80000 | ...    | start fake rom parts, but b9 use this part  |
+# | 0x80000 | 0x2000 | fake rom's lv2 blowfish data                |
+# | 0x82000 | 0x1000 | nds header                                  |
+# | 0x83600 | 0x48   | first 0x48 bytes of fake rom's blowfish     |
+# | 0x84800 | 0x1000 | next 0xF78 bytes, but these not important   |
+# | 0x86000 | 0x4000 | sec block                                   |
+# | 0x8A000 | ...    | data block                                  |
 AK2I_BLOWFISH_OFFSET = 0x80000
 AK2I_HEADER_OFFSET = 0x82000
 AK2I_1ST_BLOWFISH_OFFSET = 0x83600
@@ -28,14 +35,12 @@ def bswap32(n):
     )
 
 def extract_key_from_boot11(fn):
+    # ref: 0xFFFF82D0 of b11
     out = ''
     with open(fn, 'rb') as r:
         boot11 = r.read()
         buf = boot11[0xB498:0xB498 + 0x1000]
         return buf
-        #out += boot11[0xC878:0xC878 + 0x800]
-        #out += buf[0x800:]
-        #out = boot11[0xA450:0xA450 + 0x1048]
     return out
 
 def make_blowfish_data(src, dest):
@@ -49,7 +54,6 @@ def make_blowfish_data(src, dest):
             return
         for x in xrange(0x48):
             idx = 0x100 * (x % 16) + buf[x]
-            #print idx, hex(idx), hex(buf[x]), hex(buf[idx])
             bf[x] = buf[idx]
     bf = bf[:0x48] + buf
 
@@ -103,8 +107,8 @@ def crypt_down(hash_table, buf, offset=0):
 def inject_firm(blowfish, firm, buf):
     # secure block order will shuffled using 0xFFFFE184
     # ref: 0xFFFF9B70
-    # 0x7E00: firm header offset
-    #decrypted = 'encryObj' + '\x00' * (0xE00 - 8)
+    # zero fill will avoid encryption; gbatek.htm#dscartridgesecurearea
+    # 0x7E00(0x9E00): firm header offset
     decrypted = '\x00' * 0xE00
     decrypted += firm[:0x200]
     secure = decrypted * 4
@@ -116,7 +120,7 @@ def inject_firm(blowfish, firm, buf):
             0
             | (1 << 27)     # NTRCARD_CLK_SLOW
             #| (1 << 22)     # NTRCARD_SEC_CMD
-            #| (0x18 << 16) # NTRCARD_DELAY2(0x18)
+            #| (0x18 << 16)  # NTRCARD_DELAY2(0x18)
             #| (1 << 14)     # NTRCARD_SEC_EN
             #| (1 << 13)     # NTRCARD_SEC_DAT
             | 0x18          # NTRCARD_DELAY1(0x18)
@@ -134,10 +138,7 @@ def inject_firm(blowfish, firm, buf):
         (0x0D7E << 16) | secure_crc,
     ]
 
-    #key_datas = buf_to_int_list(blowfish)
     firm_sections = buf_to_int_list(firm[0x200:])
-    #for x in xrange(0, len(firm_sections), 2):
-    #    crypt_down(key_datas, firm_sections, x)
     firm_sections = int_list_to_buf(firm_sections)
 
     header = (
@@ -151,23 +152,14 @@ def inject_firm(blowfish, firm, buf):
         buf[:AK2I_BLOWFISH_OFFSET] +
         blowfish +
         buf[AK2I_BLOWFISH_OFFSET + 0x1048:AK2I_HEADER_OFFSET] +
-        #buf[:AK2I_HEADER_OFFSET] +
-        #buf[AK2I_HEADER_OFFSET:AK2I_HEADER_OFFSET + 0x60] +
-        #'\x00' * 0x10 +
-        #int_list_to_buf(flags) +
-        #buf[AK2I_HEADER_OFFSET + 0x68:AK2I_1ST_BLOWFISH_OFFSET] +
-        #buf[AK2I_HEADER_OFFSET + 0x70:AK2I_1ST_BLOWFISH_OFFSET] +
         header +
         buf[AK2I_HEADER_OFFSET + 0x160:AK2I_1ST_BLOWFISH_OFFSET] +
-        #blowfish[:0x48] +
         buf[:0x48] +
         buf[AK2I_1ST_BLOWFISH_OFFSET + 0x48:AK2I_2ND_BLOWFISH_OFFSET] +
-        #blowfish[0x48:0x48 + 0xBF0] +
         buf[0x48:0x48 + 0xBF0] +
         buf[AK2I_2ND_BLOWFISH_OFFSET + 0xBF0:AK2I_SEC_OFFSET] +
         secure +
         buf[AK2I_SEC_END_OFFSET:AK2I_DATA_OFFSET] +
-        #firm[0x200:] +
         firm_sections +
         buf[AK2I_DATA_OFFSET + len(firm) - 0x200:]
     )
